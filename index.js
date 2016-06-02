db9cdc7e162796722298e6214a307b0c53cd3fd3
liuhengchong <liuhengchong@southzw.com>
2016-06-02 11:41:06 +0800

    add

---
+++ index.js
@@ -0,0 +1,25 @@
+var cp = require('child_process'),
+    io = require('socket.io'),
+    spawn = cp.spawn,
+    io = io.listen(7891),
+    filename = 'package.json';
+
+io.sockets.on('connection', function (socket) {
+    console.log("INFO: Socket connected!");
+
+    var child = spawn('tail', ['-f', filename]);
+    child.stdout.on('data', function (data) {
+        console.log("Data sent");
+        socket.emit('data', {
+            lines: data.toString().split("\n")
+        });
+    });
+
+    child.stderr.on('data', function (data) {
+        console.log('Error: ' + data.toString());
+    });
+
+    socket.on('disconnect', function () {
+        child.kill();
+    });
+});
